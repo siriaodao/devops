c284bcdc07af850d4950c4219319cb60f3fe559c
siriaodao <siriaodao@vip.qq.com>
2017-05-08 16:04:18 +0800

    update mysql run container

--- mysql/run_mysql.sh
+++ mysql/run_mysql.sh
@@ -1,5 +1,5 @@
 # v5.7.18
-docker run -d -u mysql --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v /data/devops/mysql/data:/var/lib/mysql registry.cn-qingdao.aliyuncs.com/icommon/mysql:v5.7.18
+docker run -d --privileged --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v /data/devops/mysql/data:/var/lib/mysql registry.cn-qingdao.aliyuncs.com/icommon/mysql:v5.7.18
 
 
 # MYSQL_ROOT_HOST
