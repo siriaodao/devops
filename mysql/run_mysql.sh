0a87473ea0b7f0c69bcb350d00e7d37dfc864636
siriaodao <siriaodao@vip.qq.com>
2017-05-08 21:38:55 +0800

    add docker-compose.yml

--- mysql/run_mysql.sh
+++ mysql/run_mysql.sh
@@ -1,5 +1,5 @@
 # v5.7.18
-docker run -d --privileged --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v /data/devops/mysql/data:/var/lib/mysql registry.cn-qingdao.aliyuncs.com/icommon/mysql:v5.7.18
+docker run -d --privileged -u mysql --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -v /data/devops/mysql/data:/var/lib/mysql registry.cn-qingdao.aliyuncs.com/icommon/mysql:v5.7.18
 
 
 # MYSQL_ROOT_HOST
@@ -28,4 +28,4 @@ $ docker run --name some-mysql -e MYSQL_ROOT_PASSWORD_FILE=/run/secrets/mysql-ro
 
 
 # v5.6
-docker run -d --name mysql -p 3306:3306  -v /data/devops/mysql/data:/var/lib/mysql  registry.cn-qingdao.aliyuncs.com/icommon/mysql:v5.6
+docker run -d --privileged -u mysql --name mysql -p 3306:3306  -v /data/devops/mysql/data:/var/lib/mysql  registry.cn-qingdao.aliyuncs.com/icommon/mysql:v5.6
